9bf7b533236e0dfb8aec3c5e8c613f71717d3924
Ciaran Donegan <ciaran@Ciarans-MacBook-Pro.local>
2019-03-30 22:06:14 +0000

    Send DV tables but don't converge correctly

--- my-router.cpp
+++ my-router.cpp
@@ -4,13 +4,13 @@
 #include <unistd.h>
 #include <string.h>
 
-#define BUFFER_SIZE 200
+#define BUFFER_SIZE 2000 // This is the udp receive buffer size
 
-// usage: ./my-router [routerIdentifier]
+// usage: ./my-router [routerID]
 int main(int argc, char* argv[]) {
 
     if (argc != 2){
-        std::cout << "Bad function call \nUsage: ./my-router [routerIdentifier] " << std::endl;
+        std::cout << "Bad function call \nUsage: ./my-router [routerID] " << std::endl;
         return 0;
     }
 
@@ -20,6 +20,7 @@ int main(int argc, char* argv[]) {
 
     int portNumber = router.queryPort(routerID);
     string host = "localhost"; // all routers run on localhost i.e. 127.0.0.1
+
 
     try {
         // Create io service
@@ -34,11 +35,11 @@ int main(int argc, char* argv[]) {
             perror("fork failed");
             return 0;
         }
-        else if (pid==0){ // This is child process - send to each neighbour periodically
 
+        // This is child process - send DV advertisementsto each neighbour periodically
+        else if (pid==0){ 
             while(1){
                 string destPort;
-
                 for (int i=0; i<6; i++){
                     // if the routers are direct neighbours
                     if(router.neighbour_costs[i] != 99 && router.neighbour_costs[i] != 0){
@@ -55,18 +56,15 @@ int main(int argc, char* argv[]) {
 
                         // initiate contact with the remote endpoint
                         socket.open(boost::asio::ip::udp::v4());
-                        string message = "I am router ";
-                        message.push_back(routerID);
+                        string message = router.ConstructMessage();
                         socket.send_to(boost::asio::buffer(message), receiver_endpoint);
                     }
                 }
-
-                sleep(5); // wait 2 seconds
+                sleep(1); // wait 1 seconds
             }
-
         }
-        else{ // This is parent process - listen for advertisements
-            
+        // This is parent process - listen for advertisements
+        else { 
             cout << "Router " << routerID << " is running" << endl;
             while(1) { // forever
                 boost::array<char, BUFFER_SIZE> recv_buf;
@@ -75,8 +73,17 @@ int main(int argc, char* argv[]) {
                 // Receive message
                 boost::system::error_code error;
                 size_t len = socket.receive_from(boost::asio::buffer(recv_buf), remote_endpoint, 0, error);
-                //cout << "Message from: " << remote_endpoint.port() << " of size " << remote_endpoint.size() << endl;
                 cout.write(recv_buf.data(), len) << endl;
+                int * neighbour_table = router.ParseMessage(recv_buf.data());
+                char neighbour;
+                // update neighbour to reflect what table we just recieved
+                for (int i=0; i<6; i++){
+                    if (neighbour_table[i] == 0)
+                        neighbour = i + 'A';
+                }
+                router.BellmanFord(neighbour, neighbour_table);
+                router.PrintRoutingTable();
+                router.StoreRoutingTable();
             }
         }
     }
@@ -86,81 +93,81 @@ int main(int argc, char* argv[]) {
 
 
 
-//     //create a DVRouter object for each router in the network
-//     DVRouter A ('A');
-//     DVRouter B ('B');
-//     DVRouter C ('C');
-//     DVRouter D ('D');
-//     DVRouter E ('E');
-//     DVRouter F ('F');
+    // //create a DVRouter object for each router in the network
+    // DVRouter A ('A');
+    // DVRouter B ('B');
+    // DVRouter C ('C');
+    // DVRouter D ('D');
+    // DVRouter E ('E');
+    // DVRouter F ('F');
     
-//     //print out the initial routing table for each router
-//     cout << "--------------------INITIAL ROUTING TABLES--------------------\n";
-//     A.PrintRoutingTable();
-//     B.PrintRoutingTable();
-//     C.PrintRoutingTable();
-//     D.PrintRoutingTable();
-//     E.PrintRoutingTable();
-//     F.PrintRoutingTable();
+    // //print out the initial routing table for each router
+    // cout << "--------------------INITIAL ROUTING TABLES--------------------\n";
+    // A.PrintRoutingTable();
+    // B.PrintRoutingTable();
+    // C.PrintRoutingTable();
+    // D.PrintRoutingTable();
+    // E.PrintRoutingTable();
+    // F.PrintRoutingTable();
 
-//     //store the initial routing table for each router
-//     A.StoreRoutingTable();
-//     B.StoreRoutingTable();
-//     C.StoreRoutingTable();
-//     D.StoreRoutingTable();
-//     E.StoreRoutingTable();
-//     F.StoreRoutingTable();
+    // //store the initial routing table for each router
+    // A.StoreRoutingTable();
+    // B.StoreRoutingTable();
+    // C.StoreRoutingTable();
+    // D.StoreRoutingTable();
+    // E.StoreRoutingTable();
+    // F.StoreRoutingTable();
 
-//     //This simulates the an exchange of messages between each router in the network and its neighbours
-//     //Each router will receive distance vectors (least cost arrays) from each of its direct neighbours and use the values in these arrays to find a new least cost path
-//     //as more rounds of messages are exchanged, the routing table for each router will converge to a steady state
-//     for(int i=0; i<3; i++){
-//         //A receives messages from its neighbours A and E
-//         A.BellmanFord('B', B.getCosts());
-//         A.BellmanFord('E', E.getCosts());
+    // //This simulates the an exchange of messages between each router in the network and its neighbours
+    // //Each router will receive distance vectors (least cost arrays) from each of its direct neighbours and use the values in these arrays to find a new least cost path
+    // //as more rounds of messages are exchanged, the routing table for each router will converge to a steady state
+    // for(int i=0; i<3; i++){
+    //     //A receives messages from its neighbours A and E
+    //     A.BellmanFord('B', B.getCosts());
+    //     A.BellmanFord('E', E.getCosts());
 
-//         //B receives distance vectors from A, C, E and F
-//         B.BellmanFord('A', A.getCosts());
-//         B.BellmanFord('C', C.getCosts());
-//         B.BellmanFord('E', E.getCosts());
-//         B.BellmanFord('F', F.getCosts());
+    //     //B receives distance vectors from A, C, E and F
+    //     B.BellmanFord('A', A.getCosts());
+    //     B.BellmanFord('C', C.getCosts());
+    //     B.BellmanFord('E', E.getCosts());
+    //     B.BellmanFord('F', F.getCosts());
 
-//         //C receives messages from its neighbours B, D and F
-//         C.BellmanFord('B', B.getCosts());
-//         C.BellmanFord('D', D.getCosts());
-//         C.BellmanFord('F', F.getCosts());
+    //     //C receives messages from its neighbours B, D and F
+    //     C.BellmanFord('B', B.getCosts());
+    //     C.BellmanFord('D', D.getCosts());
+    //     C.BellmanFord('F', F.getCosts());
 
-//         //D receives messages from its neighbours C and F
-//         D.BellmanFord('C', C.getCosts());
-//         D.BellmanFord('F', F.getCosts());
+    //     //D receives messages from its neighbours C and F
+    //     D.BellmanFord('C', C.getCosts());
+    //     D.BellmanFord('F', F.getCosts());
 
-//         //E receives messages from its neighbours A, B and F
-//         E.BellmanFord('A', A.getCosts());
-//         E.BellmanFord('B', B.getCosts());
-//         E.BellmanFord('F', F.getCosts());
+    //     //E receives messages from its neighbours A, B and F
+    //     E.BellmanFord('A', A.getCosts());
+    //     E.BellmanFord('B', B.getCosts());
+    //     E.BellmanFord('F', F.getCosts());
 
-//         //F receives messages from its neighbours B, C, D and E
-//         F.BellmanFord('B', B.getCosts());
-//         F.BellmanFord('C', C.getCosts());
-//         F.BellmanFord('D', D.getCosts());
-//         F.BellmanFord('E', E.getCosts());
+    //     //F receives messages from its neighbours B, C, D and E
+    //     F.BellmanFord('B', B.getCosts());
+    //     F.BellmanFord('C', C.getCosts());
+    //     F.BellmanFord('D', D.getCosts());
+    //     F.BellmanFord('E', E.getCosts());
 
-//         //print out the routing table for each router after each exchange of messages
-//         cout << "--------------------ROUTING TABLES AFTER EXCHANGE " << i+1 << "--------------------\n";
-//         A.PrintRoutingTable();
-//         B.PrintRoutingTable();
-//         C.PrintRoutingTable();
-//         D.PrintRoutingTable();
-//         E.PrintRoutingTable();
-//         F.PrintRoutingTable();
+    //     //print out the routing table for each router after each exchange of messages
+    //     cout << "--------------------ROUTING TABLES AFTER EXCHANGE " << i+1 << "--------------------\n";
+    //     A.PrintRoutingTable();
+    //     B.PrintRoutingTable();
+    //     C.PrintRoutingTable();
+    //     D.PrintRoutingTable();
+    //     E.PrintRoutingTable();
+    //     F.PrintRoutingTable();
 
-//         //store the routing table for each router
-//         A.StoreRoutingTable();
-//         B.StoreRoutingTable();
-//         C.StoreRoutingTable();
-//         D.StoreRoutingTable();
-//         E.StoreRoutingTable();
-//         F.StoreRoutingTable();
-//     }
+    //     //store the routing table for each router
+    //     A.StoreRoutingTable();
+    //     B.StoreRoutingTable();
+    //     C.StoreRoutingTable();
+    //     D.StoreRoutingTable();
+    //     E.StoreRoutingTable();
+    //     F.StoreRoutingTable();
+    // }
 
 }
