2f6e2cc172d99bb7badb5a6492a744968921831e
Ciaran Donegan <ciaran@Ciarans-MacBook-Pro.local>
2019-04-04 21:14:41 +0100

    DataSegment class header

---
+++ DataSegment.hpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+// Header will be used to direct the message to its destination and provide information about its purpose
+typedef struct Header Header;
+struct Header {
+    char status; // distinguish between a data packet (status='D') and a control message (status='C')
+    int sourcePort;
+    int destPort;
+    int payloadLength; // length of payload message
+};
+
+
+/*         Structure of data segment       
+*  -----------------------------------------
+* | status | sourcePort | destPort | length |
+*  -----------------------------------------
+* |                                         |
+* |             payload                     |
+* |                                         |
+*  -----------------------------------------
+*/
+
+class DataSegment{
+    private:
+        Header header;
+        string payload;
+    public:
+        // Overloaded constructor to create data segment
+        // Will be used on the sending side
+        DataSegment(char status, int sourcePort, int destPort, int length, string payload);
+
+        // Overloaded constructor which takes a string representing a data segment 
+        // (i.e. a string of the data segment structute above) and parses it into a data segment object
+        // Will be used on the receiving side
+        DataSegment(string dataSegment);
+
+        // returns a string of data segment in the format of the structure above
+        string constructDataSegment();
+        
+        // This is mostly for testing purposes
+        void printDataSegment();
+};
