ef7256e9135d12a5647de903428eb3ba719cd1e6
Ciaran Donegan <ciaran@Ciarans-MacBook-Pro.local>
2019-04-06 08:15:57 +0100

    Added startup script and updated ReadMe

---
+++ startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+xterm -title "A" -hold -e "./my-router A"  &
+xterm -title "B" -hold -e "./my-router B"  &
+xterm -title "C" -hold -e "./my-router C"  &
+xterm -title "D" -hold -e "./my-router D"  &
+xterm -title "E" -hold -e "./my-router E"  &
+xterm -title "F" -hold -e "./my-router F" 
