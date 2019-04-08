898c72053e5f031debc346c520b4f4867a3c347d
Ciaran Donegan <ciaran@Ciarans-MacBook-Pro.local>
2019-04-08 23:10:12 +0100

    Data packet gets routed correctly

--- DV_router.hpp
+++ DV_router.hpp
@@ -18,7 +18,7 @@ private:
     char destinations[6];       //identifiers of all destination routers in the network
     char next_hop[6];           //identifier of the next-hop routers in the least-cost path
     int next_hop_port[6];       //port number of next-hop router in the least cost path
-
+    
 public:
     int neighbour_costs[6];                                         //link costs to the router's immediate neighbours
 
@@ -29,7 +29,9 @@ public:
 
     int queryPort(char dest_name);                                  //find the UDP port number associated with the router's name
     int queryID(int port);                                          //find the router identifier associated with the router port number
-
+ 
+    int getNextHopPort(int destRouterID){return next_hop_port[destRouterID - 'A'];}                      
+    
     void PrintRoutingTable(double curr_time, int * rcvd_table, char ID);     //print the router's routing/forwarding table to the terminal
     void StoreRoutingTable(double curr_time, int * rcvd_table, char ID);     //store the router's routing/forwarding table in a text file
 
--- my-router.cpp
+++ my-router.cpp
@@ -8,6 +8,8 @@
 
 #define BUFFER_SIZE 8192 // This is the udp receive buffer size
 
+void trafficGenerator();
+
 // usage: ./my-router [routerID]
 int main(int argc, char* argv[]) {
 
@@ -17,6 +19,15 @@ int main(int argc, char* argv[]) {
     }
 
     char routerID = *argv[1];
+
+    // If routerID = 'H' then this router is the traffic generator
+    // It will send one data packet and then exit. It will not participate in DV routing.
+    if(routerID == 'H'){
+        trafficGenerator();
+        return 0;
+    }
+
+
     DVRouter router(routerID); // create DV router object
     time_t start = time(0); // time = 0 when router is created
     double seconds_since_start = 0;
@@ -75,7 +86,6 @@ int main(int argc, char* argv[]) {
         }
         // This is parent process - listen for advertisements
         else { 
-            //cout << "Router " << routerID << " is running" << endl;
             while(1) { // forever
                 boost::array<char, BUFFER_SIZE> recv_buf;
                 // The remote_endpoint object will be populated by boost::asio::ip::udp::socket::receive_from().
@@ -101,13 +111,79 @@ int main(int argc, char* argv[]) {
                             //router.StoreRoutingTable(seconds_since_start, neighbour_table, neighbour);
                         }
                     }
+                    // must be a data packet that we have to route through the network
+                    else if (packetIn.getStatus() == 'D'){
+                        int destPort = packetIn.getDestPort(); // what is the ultimate destination of the data packet
+                        // if it is this router, job done!
+                        if (destPort == portNumber){
+                            packetIn.printDataSegment();
+                        }
+                        else { // otherwise forward it on to the next hop in shortest path
+                            int destID = router.queryID(destPort);
+                            int nextHop = router.getNextHopPort(destID);
+                            cout << "Next Hop: " << nextHop << endl;
+
+                            // forward on to next hop router
+                            // resolver object to find the correct remote endpoint to use based on the host and service names
+                            boost::asio::ip::udp::resolver resolver(io_service);
+                            // Make query to the UDP (ipv4) host server
+                            boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), host, to_string(nextHop));
+                            boost::asio::ip::udp::endpoint receiver_endpoint = *resolver.resolve(query);
+                            // Using datagram socket, not stream socket.
+                            boost::asio::ip::udp::socket socket(io_service);
+                            // initiate contact with the remote endpoint
+                            socket.open(boost::asio::ip::udp::v4());
+                            // construct a string of data segment and send over udp
+                            socket.send_to(boost::asio::buffer(packetIn.constructDataSegment()), receiver_endpoint);
+                        }
+                        
+                    }
                 }
             }
         }
     }
     
-    catch (std::exception &e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
+    catch (exception &e) {
+        cerr << "Exception: " << e.what() << endl;
     }
 
+    return 0;
+}
+
+
+void trafficGenerator(){
+
+    string payload = "This is a data packet. Let's see if it can make it's way from router A to F";
+    // create a data segment object with status = 'D' to denote a data packet
+    int originPort = 10000; // A
+    int destinationPort = 10005; // F
+
+    DataSegment dataPacket('D', originPort, destinationPort, payload.length(), payload);
+    // print the datapacket to console
+    dataPacket.printDataSegment();
+    int portNo = 10010;
+    string host = "localhost"; // all routers run on localhost i.e. 127.0.0.1
+    string initialPort = to_string(originPort); // start at router A on port 10000
+
+    try{
+        // Create io service
+        boost::asio::io_service io_service;
+        // create a UDP socket object on portNumber
+        boost::asio::ip::udp::socket socket(io_service, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), portNo));
+        // resolver object to find the correct remote endpoint to use based on the host and service names
+        boost::asio::ip::udp::resolver resolver(io_service);
+        // Make query to the UDP (ipv4) host server
+        boost::asio::ip::udp::resolver::query query(boost::asio::ip::udp::v4(), host, initialPort);
+        // Will return at least one endpoint in the list if it does not fail.
+        // This means it is safe to dereference the return value directly.
+        boost::asio::ip::udp::endpoint receiver_endpoint = *resolver.resolve(query);
+
+        // construct a string of data segment and send over udp
+        socket.send_to(boost::asio::buffer(dataPacket.constructDataSegment()), receiver_endpoint);
+    }
+    catch (exception &e) {
+        cerr << "Exception in trafficGenerator(): " << e.what() << endl;
+    }
+
+    return;
 }
